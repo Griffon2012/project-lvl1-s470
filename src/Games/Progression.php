704a567af51f50529be617ca82cb0a4970bbb23a
Griffon2012 <trifonov-ga@yandex.ru>
2019-04-07 20:50:58 +0300

    fix progression

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -11,9 +11,11 @@ function run()
     $getQuestionAndAnswer = function () {
         $step = rand(1, 5);
         $start = rand(1, 25);
-        $hiddenIndexNumber = rand(0, (LENGTH_PROGRESSION - 1));
-        $correctAnswer = (string) ($step * $hiddenIndexNumber + $start);
-        $question = implode(' ', getProgressionWithHiddenElement($start, $step, $hiddenIndexNumber));
+        $progression = getProgression($start, $step);
+        $hiddenIndexNumber = array_rand($progression);
+        $correctAnswer = $progression[$hiddenIndexNumber];
+        $progressionWithHiddenElement = array_replace($progression, [$hiddenIndexNumber => '..']);
+        $question = implode(' ', $progressionWithHiddenElement);
     
         return [$question, $correctAnswer];
     };
@@ -21,15 +23,11 @@ function run()
     startGame(ANNOTATION, $getQuestionAndAnswer);
 }
 
-function getProgressionWithHiddenElement($start, $step, $hiddenIndexNumber)
+function getProgression($start, $step)
 {
     $progression = [];
     for ($i = 0; $i < LENGTH_PROGRESSION; $i++) {
-        if ($i === $hiddenIndexNumber) {
-            $progression[] = '..';
-        } else {
-            $progression[] = (string) ($start + $step * $i);
-        }
+        $progression[] = (string) ($start + $step * $i);
     }
     return $progression;
 }
